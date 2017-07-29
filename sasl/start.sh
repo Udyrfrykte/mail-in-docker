9143cff70a2627293c294cad603b4d9f0697ce56
Udyrfrykte <Udyrfrykte@users.noreply.github.com>
2017-07-29 20:31:40 +0200

    some rework on dovecot components and addition of a LDAP server to handle authentication through binds on this LDAP server

--- sasl/start.sh
+++ sasl/start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
-# Configure passwd.db
-echo "${PASSWD_DB}" >| /etc/dovecot/passwd.db
+parameters=(
+  "USER_DN_TEMPLATE"
+)
+
+export USER_DN_TEMPLATE="${USER_DN_TEMPLATE:-uid=%n,ou=People,dc=example,dc=com}"
+
+# Substitute configuration
+for VARIABLE in ${parameters[@]}; do
+  sed -i "s#{{ $VARIABLE }}#${!VARIABLE}#g" /etc/dovecot/dovecot-ldap.conf
+done
 
 # Run dovecot
 exec /usr/sbin/dovecot -c /etc/dovecot/dovecot.conf -F
