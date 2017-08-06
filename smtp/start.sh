25b281a60ca43fee09062020c2c1b97391279941
Udyrfrykte <Udyrfrykte@users.noreply.github.com>
2017-08-06 20:49:54 +0200

    use mdap for SMTP users and aliases

--- smtp/start.sh
+++ smtp/start.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 set -e
 
+parameters=(
+  "DOMAIN"
+  "BIND_DN"
+  "BIND_PW"
+  "USERS_SEARCH_BASE"
+  "ALIASES_SEARCH_BASE"
+)
+
 # Configure aliases
-echo "${ALIASES}" >| /etc/postfix/virtual-aliases
 echo "${ALIASES_REGEX}" >| /etc/postfix/virtual-aliases-re
 
 export DOMAIN="${DOMAIN:-example.com}"
 
 # Substitute configuration
-VARIABLES=(
-  DOMAIN
-)
-for VARIABLE in "${VARIABLES[@]}"; do
-  sed -i "s={{ $VARIABLE }}=${!VARIABLE}=g" /etc/postfix/main.cf
-  sed -i "s={{ $VARIABLE }}=${!VARIABLE}=g" /etc/aliases
-  sed -i "s={{ $VARIABLE }}=${!VARIABLE}=g" /etc/postfix/canonical
-  sed -i "s={{ $VARIABLE }}=${!VARIABLE}=g" /etc/postfix/virtual-mailbox-domains
+for VARIABLE in "${parameters[@]}"; do
+  sed -i "s#{{ $VARIABLE }}#${!VARIABLE}#g" /etc/postfix/main.cf
+  sed -i "s#{{ $VARIABLE }}#${!VARIABLE}#g" /etc/aliases
+  sed -i "s#{{ $VARIABLE }}#${!VARIABLE}#g" /etc/postfix/canonical
+  sed -i "s#{{ $VARIABLE }}#${!VARIABLE}#g" /etc/postfix/virtual-mailbox-domains
+  sed -i "s#{{ $VARIABLE }}#${!VARIABLE}#g" /etc/postfix/ldap-users.cf
+  sed -i "s#{{ $VARIABLE }}#${!VARIABLE}#g" /etc/postfix/ldap-aliases.cf
 done
-
-newaliases
-
-# generate user configuration
-echo "${USERS}" | sed "s/\(.*\)/\1@${DOMAIN} \1@${DOMAIN}/" >| /etc/postfix/virtual-mailbox-users
 
 # launch rsyslog
 rm -f /var/run/rsyslogd.pid
